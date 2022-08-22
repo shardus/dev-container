d0432e2ae06585a8f09d05f6fb5bd12891145dc4
Aamir Syed <asyed94@gmail.com>
2022-08-22 15:43:23 -0500

    Uncomments python and rust features and adds prettier to extensions.

--- update-container.sh
+++ update-container.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-devcontainer build --docker-path podman-remote --image-name registry.gitlab.com/shardus/dev-container --workspace-folder `pwd`
+# [AS] On Silverblue, the devcontainer/cli is installed in a distrobox container (fedora) which needs to have podman-remote installed:
+#   sudo dnf install podman-remote
+# and the --docker-path needs to be podman-remote
+devcontainer build --docker-path podman-remote --image-name registry.gitlab.com/shardus/dev-container --workspace-folder $(pwd)
+podman login registry.gitlab.com/shardus/dev-container
 podman push registry.gitlab.com/shardus/dev-container
