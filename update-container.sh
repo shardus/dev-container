8a3d8f111b4128c67c6688be19791e304fbf1a78
Aamir Syed <asyed94@gmail.com>
2023-07-25 12:15:13 -0500

    Updates debian version in devcontainer.json to fix build. Adds `latest_node18` tag to update-container.sh script

--- update-container.sh
+++ update-container.sh
@@ -2,6 +2,6 @@
 # [AS] On Silverblue, the devcontainer/cli is installed in a distrobox container (fedora) which needs to have podman-remote installed:
 #   sudo dnf install podman-remote
 # and the --docker-path needs to be podman-remote
-devcontainer build --docker-path podman-remote --image-name registry.gitlab.com/shardus/dev-container --workspace-folder $(pwd)
-podman login registry.gitlab.com/shardus/dev-container
-podman push registry.gitlab.com/shardus/dev-container
+devcontainer build --docker-path podman-remote --image-name registry.gitlab.com/shardus/dev-container:latest_node18 --workspace-folder $(pwd)
+podman-remote login registry.gitlab.com/shardus/dev-container
+podman-remote push registry.gitlab.com/shardus/dev-container:latest_node18
